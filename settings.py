735ac7b20f2e32047facc2900e69eebc4734868a
ravenusmc <mcuddy77@gmail.com>
2016-05-30 17:08:39 -0400

    added image for game

--- settings.py
+++ settings.py
@@ -5,5 +5,6 @@ class Settings():
     #init the games settings.
     #screen settings
     self.screen_width = 1200
-    self.screen_height = 600
+    self.screen_height = 500
     self.bg_color = (230, 230, 230)
+
