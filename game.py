a5826026da2f3d543e89f696b35cc4abf66f641c
ravenusmc <mcuddy77@gmail.com>
2016-06-01 10:41:28 -0400

    refractured code in game

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
-import sys
 import pygame
+import game_functions as gf
 
 from settings import Settings
 from ship import Ship
@@ -16,19 +16,8 @@ def run_game():
 
   #Start the main loop for the game.
   while True:
-
-    #Watch for keyboard and mouse events
-    for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-        sys.exit()
-
-    #Redraw the screen during each pass through the loop
-    screen.fill(ai_settings.bg_color)
-    ship.blitme()
-
-
-    #Make the most recently drawn screen visible.
-    pygame.display.flip()
+    gf.check_events()
+    gf.update_screen(ai_settings, screen, ship)
 
 
 run_game()
