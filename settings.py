cfb38a8fb0977a10048a5e5033d23f7ddfdc5e26
ravenusmc <mcuddy77@gmail.com>
2016-06-02 11:30:52 -0400

    increased movement speed of ship

--- settings.py
+++ settings.py
@@ -7,4 +7,6 @@ class Settings():
     self.screen_width = 1000
     self.screen_height = 700
     self.bg_color = (230, 230, 230)
+    #ship settings 
+    self.ship_speed_factor = 1.5 
 
--- ship.py
+++ ship.py
@@ -2,10 +2,11 @@ import pygame
 
 class Ship():
 
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
 
         #init the ship and set its starting position.
         self.screen = screen
+        self.ai_settings = ai_settings
 
         #Load the ship image and gets its rect.
         self.image = pygame.image.load('images/ship.bmp')
@@ -16,6 +17,9 @@ class Ship():
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
+        #Store a decimal value for the ship's center.
+        self.center = float(self.rect.centerx)
+
         #Movement Flag
         self.moving_right = False
         self.moving_left = False
@@ -23,9 +27,12 @@ class Ship():
     def update(self):
         #update the ships position based on the movement flag
         if self.moving_right:
-            self.rect.centerx += 1 
+            self.center += self.ai_settings.ship_speed_factor 
         if self.moving_left:
-            self.rect.centerx -= 1
+            self.center -= self.ai_settings.ship_speed_factor
+
+        #Update rect object from self.center
+        self.rect.centerx = self.center
 
     def blitme(self):
 
