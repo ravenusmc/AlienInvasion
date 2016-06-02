b8e34ac4fb507921b1932ced5d537c06b524b66d
ravenusmc <mcuddy77@gmail.com>
2016-06-02 11:39:43 -0400

    refractured some ship movement code

--- game_functions.py
+++ game_functions.py
@@ -1,25 +1,31 @@
 import sys
 import pygame 
 
+def check_keydown_events(event, ship):
+  #Responds to key presses
+  if event.key == pygame.K_RIGHT:
+    #moves the ship right.
+    ship.moving_right = True
+  elif event.key == pygame.K_LEFT:
+    ship.moving_left = True 
+
+def check_keyup_events(event, ship):
+  #Responds to key releases
+  if event.key == pygame.K_RIGHT:
+    ship.moving_right = False
+  elif event.key == pygame.K_LEFT:
+    ship.moving_left = False
+
 def check_events(ship):
   #Respond to keypresses and mouse events 
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       sys.exit()
-
     elif event.type == pygame.KEYDOWN:
-      if event.key == pygame.K_RIGHT:
-        #moves the ship right.
-        ship.moving_right = True
-      elif event.key == pygame.K_LEFT:
-        #moves the ship to the left.
-        ship.moving_left = True
+      check_keydown_events(event, ship)
     elif event.type == pygame.KEYUP:
-        #stops the ship from moving left and right.
-        if event.key == pygame.K_RIGHT:
-          ship.moving_right = False
-        elif event.key == pygame.K_LEFT:
-          ship.moving_left = False
+      check_keyup_events(event, ship)
+
 
 def update_screen(ai_settings, screen, ship):
     #Redraw the screen during each pass through the loop
@@ -29,3 +35,5 @@ def update_screen(ai_settings, screen, ship):
 
     #Make the most recently drawn screen visible.
     pygame.display.flip()
+
+
