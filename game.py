7e023fd26afeba82d1f9742b80c51f3dbb89bca3
ravenusmc <mcuddy77@gmail.com>
2016-06-03 10:45:18 -0400

    ship can now shoot

--- game.py
+++ game.py
@@ -3,6 +3,7 @@ import game_functions as gf
 
 from settings import Settings
 from ship import Ship
+from pygame.sprite import Group 
 
 def run_game():
   #inits game and creates a screen object.
@@ -13,12 +14,16 @@ def run_game():
 
   #make a ship
   ship = Ship(ai_settings, screen)
+  #Make a group to store bullets in. 
+  bullets = Group()
+
 
   #Start the main loop for the game.
   while True:
-    gf.check_events(ship)
+    gf.check_events(ai_settings, screen, ship, bullets)
     ship.update()
-    gf.update_screen(ai_settings, screen, ship)
+    bullets.update()
+    gf.update_screen(ai_settings, screen, ship, bullets)
 
 
 run_game()
