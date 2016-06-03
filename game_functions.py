39375e60012d56dbfadf52e33ff97a6c6bccd390
ravenusmc <mcuddy77@gmail.com>
2016-06-03 11:10:28 -0400

    refractured code

--- game_functions.py
+++ game_functions.py
@@ -11,9 +11,14 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
   elif event.key == pygame.K_LEFT:
     ship.moving_left = True 
   elif event.key == pygame.K_SPACE:
-    #Create a new bullet and add it to the bullets group. 
-    new_bullet = Bullet(ai_settings, screen, ship)
-    bullets.add(new_bullet)
+    fire_bullet(ai_settings, screen, ship, bullets)
+
+def fire_bullet(ai_settings, screen, ship, bullets):
+  #Fire a bullet if limit not reached yet.
+  #Create a new bullet and add it to the bullets group.
+    if len(bullets) < ai_settings.bullets_allowed:
+      new_bullet = Bullet(ai_settings, screen, ship)
+      bullets.add(new_bullet)
 
 def check_keyup_events(event, ship):
   #Responds to key releases
@@ -40,6 +45,17 @@ def update_screen(ai_settings, screen, ship, bullets):
     for bullet in bullets.sprites():
       bullet.draw_bullet()
     ship.blitme()
+
+def update_bullets(bullets):
+  #Update position of bullets and get rid of old bullets
+  #Update bullet positions 
+  bullets.update()
+
+  #Get rid of bullets that have disappeared 
+  for bullet in bullets.copy():
+    if bullet.rect.bottom <= 0:
+      bullets.remove(bullet)
+
 
 
     #Make the most recently drawn screen visible.
--- settings.py
+++ settings.py
@@ -8,10 +8,11 @@ class Settings():
     self.screen_height = 700
     self.bg_color = (230, 230, 230)
     #ship settings 
-    self.ship_speed_factor = 5
+    self.ship_speed_factor = 8
     #bullet settings
-    self.bullet_speed_factor = 1
+    self.bullet_speed_factor = 10
     self.bullet_width = 3
     self.bullet_height = 15
     self.bullet_color = 60,60,60
+    self.bullets_allowed = 3
 
