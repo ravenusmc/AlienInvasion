5cb8f0b22c4eddf2a4d326a2f058ff54a3e659f6
ravenusmc <mcuddy77@gmail.com>
2016-06-13 13:43:56 -0400

    added better tracking for score

--- game_functions.py
+++ game_functions.py
@@ -97,7 +97,7 @@ def update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_bu
     # Make the most recently drawn screen visible.
     pygame.display.flip()
 
-def update_bullets(ai_settings, screen, ship, aliens, bullets):
+def update_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets):
   #Update position of bullets and get rid of old bullets
   #Update bullet positions 
   bullets.update()
@@ -108,12 +108,16 @@ def update_bullets(ai_settings, screen, ship, aliens, bullets):
     if bullet.rect.bottom <= 0:
       bullets.remove(bullet)
 
-  check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets)
+  check_bullet_alien_collisions(ai_settings, screen, stats, sb, ship, aliens, bullets)
 
-def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
+def check_bullet_alien_collisions(ai_settings, screen, stats, sb, ship, aliens, bullets):
   #check for any bullets that have hit aliens
   #If so, get rid of the bullet and the alien.
   collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
+  if collisions: 
+    for aliens in collisions.values():
+      stats.score += ai_settings.alien_points * len(aliens)
+      sb.prep_score()
 
   if len(aliens) == 0:
     #Destroy existing bullets, speed up game and create new fleet
